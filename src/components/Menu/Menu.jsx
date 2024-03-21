68e0ee65a62ecfeeb28ef9f001886ebd40327de1
icaroboaventura <boaventura@outlook.de>
2024-03-21 16:16:27 -0300

    Add suggestions

--- src/components/Menu/Menu.jsx
+++
@@ -1,5 +0,0 @@
-const Menu = () => {
-  return <a href="">Menu</a>;
-};
-
-export default Menu;
--- src/components/PopUp/PopUp.jsx
+++
@@ -1,7 +0,0 @@
-import './PopUp.css';
-
-const PopUp = () => {
-  return <></>;
-};
-
-export default PopUp;
