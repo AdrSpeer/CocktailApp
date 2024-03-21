e863830fc2eb446f0f09adc881ad87a2b74fff88
david06186 <david.engaus@gmail.com>
2024-03-21 12:04:49 +0100

    add context & routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeContext } from './context/Context';
+import Home from './pages/Home/Home';
+import AddRecipe from './pages/AddRecipe/AddRecipe';
+import TypesPage from './pages/TypesPage/TypesPage';
 import './App.css';
 
 function App() {
-  return <></>;
+  const [theme, setTheme] = useState('');
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/add-recipe' element={<AddRecipe />} />
+          <Route path='/types' element={<TypesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
