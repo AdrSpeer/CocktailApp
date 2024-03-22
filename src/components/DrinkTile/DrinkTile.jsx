e4b9c4bd5fe94839e9035c9bb5928a703feed479
david06186 <david.engaus@gmail.com>
2024-03-22 13:46:59 +0100

    exported DrinkTiles as a component

---
+++ src/components/DrinkTile/DrinkTile.jsx
@@ -0,0 +1,21 @@
+import './DrinkTile.css';
+
+const DrinkTile = ({ props }) => {
+  return (
+    <section className='filter-grid' id='drinks'>
+      {props ? (
+        props?.map((drink) => {
+          console.log(drink.strDrink, drink.idDrink);
+          <div className='filter background-1' key={drink.idDrink}>
+            <p>{drink.strDrink}</p>
+            <img src={drink.strDrinkThumb} alt='a picture of the drink' />
+          </div>;
+        })
+      ) : (
+        <p>Loading...</p>
+      )}
+    </section>
+  );
+};
+
+export default DrinkTile;
--- src/pages/TypesPage/TypesPage.jsx
+++ src/pages/TypesPage/TypesPage.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import './TypesPage.css';
 import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
+import DrinkTile from '../../components/DrinkTile/DrinkTile';
 
 const TypesPage = () => {
   const [allDrinks, setAllDrinks] = useState();
@@ -28,36 +29,12 @@ const TypesPage = () => {
   return (
     <>
       <Header />
-      <section className='filter-grid' id='drinks'>
-        {allDrinks ? (
-          allDrinks?.map((drink) => {
-            console.log(drink.strDrink, drink.idDrink);
-            <div className='filter background-1' key={drink.idDrink}>
-              <p>{drink.strDrink}</p>
-              <img src={drink.strDrinkThumb} alt='a picture of the drink' />
-            </div>;
-          })
-        ) : (
-          <p>Loading...</p>
-        )}
-        <ErrorBoundary></ErrorBoundary>
-      </section>
+      <ErrorBoundary>
+        <DrinkTile props={allDrinks} />
+      </ErrorBoundary>
       <Footer />
     </>
   );
 };
 
 export default TypesPage;
-
-// {
-//   allDrinks ? (
-//     allDrinks?.map((item) => {
-//       <div className='filter' key={item.idDrink}>
-//         <h2>{item.strDrink}</h2>
-//         <img src={item.strDrinkThumb} alt='a picture of the drink' />
-//       </div>;
-//     })
-//   ) : (
-//     <p>Loading...</p>
-//   );
-// }
