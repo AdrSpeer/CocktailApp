ef51b016d0700ee483211b486e41fcbd3f655c62
david06186 <david.engaus@gmail.com>
2024-03-22 13:32:10 +0100

    add ErrorBoundary component

--- src/pages/TypesPage/TypesPage.jsx
+++ src/pages/TypesPage/TypesPage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import './TypesPage.css';
+import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
 
 const TypesPage = () => {
   const [allDrinks, setAllDrinks] = useState();
@@ -14,62 +15,32 @@ const TypesPage = () => {
           `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${type}`
         )
           .then((response) => response.json())
-          .then((fetchedData) => setAllDrinks(fetchedData))
+          .then((fetchedData) => setAllDrinks(fetchedData.drinks))
           .catch((error) => console.error('Error auf der Details Page', error))
       : fetch(
           `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`
         )
           .then((response) => response.json())
-          .then((fetchedData) => setAllDrinks(fetchedData))
+          .then((fetchedData) => setAllDrinks(fetchedData.drinks))
           .catch((error) => console.error('Error auf der Details Page', error));
   }, [type]);
-
-  console.log(allDrinks);
 
   return (
     <>
       <Header />
       <section className='filter-grid' id='drinks'>
-        <div className='filter background-1'>
-          <h2>Gin</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <img src='../../../public/svg/Gin.jpg' alt='' />
-        </div>
-        <div className='filter background-2'>
-          <h2>Vodka</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <img src='../../../public/svg/vodka.jpg' alt='' />
-        </div>
-        <div className='filter background-1'>
-          <h2>Gin</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <img src='../../../public/svg/Gin.jpg' alt='' />
-        </div>
-        <div className='filter background-2'>
-          <h2>Vodka</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <img src='../../../public/svg/vodka.jpg' alt='' />
-        </div>
-        <div className='filter background-1'>
-          <h2>Gin</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <img src='../../../public/svg/Gin.jpg' alt='' />
-        </div>
-        <div className='filter background-2'>
-          <h2>Vodka</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <img src='../../../public/svg/vodka.jpg' alt='' />
-        </div>
-        <div className='filter background-1'>
-          <h2>Gin</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <img src='../../../public/svg/Gin.jpg' alt='' />
-        </div>
-        <div className='filter background-2'>
-          <h2>Vodka</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <img src='../../../public/svg/vodka.jpg' alt='' />
-        </div>
+        {allDrinks ? (
+          allDrinks?.map((drink) => {
+            console.log(drink.strDrink, drink.idDrink);
+            <div className='filter background-1' key={drink.idDrink}>
+              <p>{drink.strDrink}</p>
+              <img src={drink.strDrinkThumb} alt='a picture of the drink' />
+            </div>;
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+        <ErrorBoundary></ErrorBoundary>
       </section>
       <Footer />
     </>
@@ -77,3 +48,16 @@ const TypesPage = () => {
 };
 
 export default TypesPage;
+
+// {
+//   allDrinks ? (
+//     allDrinks?.map((item) => {
+//       <div className='filter' key={item.idDrink}>
+//         <h2>{item.strDrink}</h2>
+//         <img src={item.strDrinkThumb} alt='a picture of the drink' />
+//       </div>;
+//     })
+//   ) : (
+//     <p>Loading...</p>
+//   );
+// }
