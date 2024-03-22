97b957f79db2b908de4ee7b6f7eebec51ae4f685
david06186 <david.engaus@gmail.com>
2024-03-22 14:47:21 +0100

    WiP: DrinkTiles

--- src/components/DrinkTile/DrinkTile.jsx
+++ src/components/DrinkTile/DrinkTile.jsx
@@ -1,16 +1,15 @@
-import "./DrinkTile.css";
+import './DrinkTile.css';
 
 const DrinkTile = ({ props }) => {
   return (
-    <section className="filter-grid" id="drinks">
+    <section className='filter-grid' id='drinks'>
       {props ? (
-        props?.map((drink) => {
-          console.log(drink.strDrink, drink.idDrink);
-          <div className="filter" key={drink.idDrink}>
-            <p>{drink.strDrink}</p>
-            <img src={drink.strDrinkThumb} alt="a picture of the drink" />
-          </div>;
-        })
+        props?.map((drink) => (
+          <div className='filter' key={drink.idDrink}>
+            <h3>{drink.strDrink}</h3>
+            <img src={drink.strDrinkThumb} alt='a picture of the drink' />
+          </div>
+        ))
       ) : (
         <p>Loading...</p>
       )}
