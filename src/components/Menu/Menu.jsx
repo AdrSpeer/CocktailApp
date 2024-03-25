6cb67f1617c2b6fd7f5f3c50097d86d537284b77
icaroboaventura <boaventura@outlook.de>
2024-03-25 16:14:01 -0300

    Add new pop and menu bar

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import "./Menu.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import globalFunctions from "../../functions/GlobalFunctions";
 
 const Menu = () => {
   const [menuPop, setMenuPop] = useState(false);
@@ -16,6 +19,34 @@ const Menu = () => {
         <div className="menu-div">
           <img onClick={() => popToggle()} className="menu-cross" src="../../../public/svg/Cross.svg" alt="" />
         </div>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <Link to="/add-recipe">
+          <p>Add Recipe</p>
+        </Link>
+        <div className="line"></div>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/gin#drinks`}>
+          <p>Gin</p>
+        </HashLink>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/vodka#drinks`}>
+          <p>Vodka</p>
+        </HashLink>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/rum#drinks`}>
+          <p>Rum</p>
+        </HashLink>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/scotch#drinks`}>
+          <p>Scotch</p>
+        </HashLink>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/tequila#drinks`}>
+          <p>Tequila</p>
+        </HashLink>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/bourbon#drinks`}>
+          <p>Bourbon</p>
+        </HashLink>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/non-alcoholic#drinks`}>
+          <p>Alk.frei</p>
+        </HashLink>
       </div>
     </div>
   );
