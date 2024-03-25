9d7c8a4107fb988497bc3ed18b21e897ee2e32fc
StefanHolz1997 <Stefanholz97@web.de>
2024-03-25 11:05:59 +0100

    Styling_Home_Done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,7 +28,6 @@ const Home = () => {
         >
           <div className="filter background-1">
             <h2>Gin</h2>
-            <p>Lorem, ipsum dolor.</p>
             <img src="/svg/Gin.jpg" alt="" />
           </div>
         </HashLink>
@@ -39,7 +38,6 @@ const Home = () => {
         >
           <div className="filter background-2">
             <h2>Vodka</h2>
-            <p>Lorem, ipsum dolor.</p>
             <img src="/svg/vodka.jpg" alt="" />
           </div>
         </HashLink>
@@ -50,7 +48,6 @@ const Home = () => {
         >
           <div className="filter background-3">
             <h2>Rum</h2>
-            <p>Lorem, ipsum dolor.</p>
             <img src="/svg/rum.jpg" alt="" />
           </div>
         </HashLink>
@@ -61,7 +58,6 @@ const Home = () => {
         >
           <div className="filter background-4">
             <h2>Scotch</h2>
-            <p>Lorem, ipsum dolor.</p>
             <img src="/svg/scotch.jpg" alt="" />
           </div>
         </HashLink>
@@ -70,9 +66,8 @@ const Home = () => {
           scroll={(el) => scrollWithOffset(el)}
           to={`/types/scotch#drinks`}
         >
-          <div className="filter background-4">
+          <div className="filter background-5">
             <h2>Tequila</h2>
-            <p>Lorem, ipsum dolor.</p>
             <img src="/svg/Tequila.jpg" alt="" />
           </div>
         </HashLink>
@@ -81,9 +76,8 @@ const Home = () => {
           scroll={(el) => scrollWithOffset(el)}
           to={`/types/scotch#drinks`}
         >
-          <div className="filter background-4">
+          <div className="filter background-6">
             <h2>Bourbon</h2>
-            <p>Lorem, ipsum dolor.</p>
             <img src="/svg/Bourbon.jpg" alt="" />
           </div>
         </HashLink>
@@ -92,15 +86,13 @@ const Home = () => {
           scroll={(el) => scrollWithOffset(el)}
           to={`/types/non-alcoholic#drinks`}
         >
-          <div className="filter background-5">
-            <h2>Alkoholfrei</h2>
-            <p>Lorem, ipsum dolor.</p>
+          <div className="filter background-1">
+            <h2>Alk.frei</h2>
             <img src="/svg/none.jpg" alt="" />
           </div>
         </HashLink>
-        <div className="filter background-6" onClick={filler()}>
+        <div className="filter background-2 box-question" onClick={filler()}>
           <h2>Zufall</h2>
-          <p>Lorem, ipsum dolor.</p>
           <p className="question">?</p>
         </div>
       </section>
