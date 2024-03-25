be685b820f0c7075a55c8d7854efbcf8f38d7f62
icaroboaventura <boaventura@outlook.de>
2024-03-25 14:07:13 -0300

    Add pop up in types

--- src/components/DrinkTile/DrinkTile.jsx
+++ src/components/DrinkTile/DrinkTile.jsx
@@ -1,13 +1,23 @@
-import './DrinkTile.css';
+import "./DrinkTile.css";
+import { PopContext } from "../../context/Context";
+import { DrinkContext } from "../../context/Context";
+import { useContext } from "react";
 
 const DrinkTile = ({ props }) => {
+  const { setPop } = useContext(PopContext);
+  const { drinkId, setDrinkId } = useContext(DrinkContext);
+
+  const popToggle = (nameString) => {
+    setPop(true);
+    setDrinkId(nameString);
+  };
   return (
-    <section className='drink-grid' id='drinks'>
+    <section className="drink-grid" id="drinks">
       {props ? (
         props?.map((drink) => (
-          <div className='filter' key={drink.idDrink}>
+          <div onClick={() => popToggle(drink.idDrink)} className="filter" key={drink.idDrink}>
             <h3>{drink.strDrink}</h3>
-            <img src={drink.strDrinkThumb} alt='a picture of the drink' />
+            <img src={drink.strDrinkThumb} alt="a picture of the drink" />
           </div>
         ))
       ) : (
