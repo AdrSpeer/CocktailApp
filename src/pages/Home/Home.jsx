b2a1328f36adf84bf4ea31a88269a815840fc103
icaroboaventura <boaventura@outlook.de>
2024-03-26 11:12:48 -0300

    Add zufall update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,15 @@
-import Header from '../../components/Header/Header';
-import { HashLink } from 'react-router-hash-link';
-import './Home.css';
-import Footer from '../../components/Footer/Footer';
-import Up from '../../assets/svg/up';
-import { useEffect, useState, useContext } from 'react';
-import globalFunctions from '../../functions/GlobalFunctions';
-import { DrinkContext } from '../../context/Context';
-import { PopContext } from '../../context/Context';
+import Header from "../../components/Header/Header";
+import { HashLink } from "react-router-hash-link";
+import "./Home.css";
+import Footer from "../../components/Footer/Footer";
+import Up from "../../assets/svg/up";
+import { useEffect, useState, useContext } from "react";
+import globalFunctions from "../../functions/GlobalFunctions";
+import { DrinkContext } from "../../context/Context";
+import { PopContext } from "../../context/Context";
 
 const Home = () => {
+  const [toggleRadom, setToggleRadom] = useState();
   const [randomCock, setRandomCock] = useState();
   const { setPop } = useContext(PopContext);
   const { setDrinkId } = useContext(DrinkContext);
@@ -17,87 +18,65 @@ const Home = () => {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
       .then((res) => res.json())
       .then((data) => setRandomCock(data.drinks))
-      .catch((error) => console.error('Fehler im Fetch Random', error));
-  }, []);
+      .catch((error) => console.error("Fehler im Fetch Random", error));
+  }, [toggleRadom]);
 
   const popToggle = (nameString) => {
     setPop(true);
     setDrinkId(nameString);
+    setToggleRadom((random) => !random);
   };
 
   return (
     <>
       <Header />
       <Up />
-      <section className='filter-grid' id='drinks'>
-        <HashLink
-          smooth
-          scroll={(el) => globalFunctions.scrollWithOffset(el)}
-          to={`/types/gin#drinks`}>
-          <div className='filter background-1'>
+      <section className="filter-grid" id="drinks">
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/gin#drinks`}>
+          <div className="filter background-1">
             <h2>Gin</h2>
-            <img src='/svg/Gin.jpg' alt='' />
+            <img src="/svg/Gin.jpg" alt="" />
           </div>
         </HashLink>
-        <HashLink
-          smooth
-          scroll={(el) => globalFunctions.scrollWithOffset(el)}
-          to={`/types/vodka#drinks`}>
-          <div className='filter background-2'>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/vodka#drinks`}>
+          <div className="filter background-2">
             <h2>Vodka</h2>
-            <img src='/svg/vodka.jpg' alt='' />
+            <img src="/svg/vodka.jpg" alt="" />
           </div>
         </HashLink>
-        <HashLink
-          smooth
-          scroll={(el) => globalFunctions.scrollWithOffset(el)}
-          to={`/types/rum#drinks`}>
-          <div className='filter background-3'>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/rum#drinks`}>
+          <div className="filter background-3">
             <h2>Rum</h2>
-            <img src='/svg/rum.jpg' alt='' />
+            <img src="/svg/rum.jpg" alt="" />
           </div>
         </HashLink>
-        <HashLink
-          smooth
-          scroll={(el) => globalFunctions.scrollWithOffset(el)}
-          to={`/types/scotch#drinks`}>
-          <div className='filter background-4'>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/scotch#drinks`}>
+          <div className="filter background-4">
             <h2>Scotch</h2>
-            <img src='/svg/scotch.jpg' alt='' />
+            <img src="/svg/scotch.jpg" alt="" />
           </div>
         </HashLink>
-        <HashLink
-          smooth
-          scroll={(el) => globalFunctions.scrollWithOffset(el)}
-          to={`/types/scotch#drinks`}>
-          <div className='filter background-5'>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/scotch#drinks`}>
+          <div className="filter background-5">
             <h2>Tequila</h2>
-            <img src='/svg/Tequila.jpg' alt='' />
+            <img src="/svg/Tequila.jpg" alt="" />
           </div>
         </HashLink>
-        <HashLink
-          smooth
-          scroll={(el) => globalFunctions.scrollWithOffset(el)}
-          to={`/types/scotch#drinks`}>
-          <div className='filter background-6'>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/scotch#drinks`}>
+          <div className="filter background-6">
             <h2>Bourbon</h2>
-            <img src='/svg/Bourbon.jpg' alt='' />
+            <img src="/svg/Bourbon.jpg" alt="" />
           </div>
         </HashLink>
-        <HashLink
-          smooth
-          scroll={(el) => globalFunctions.scrollWithOffset(el)}
-          to={`/types/non-alcoholic#drinks`}>
-          <div className='filter background-1'>
+        <HashLink smooth scroll={(el) => globalFunctions.scrollWithOffset(el)} to={`/types/non-alcoholic#drinks`}>
+          <div className="filter background-1">
             <h2>Alk.frei</h2>
-            <img src='/svg/none.jpg' alt='' />
+            <img src="/svg/none.jpg" alt="" />
           </div>
         </HashLink>
-        <div
-          className='filter background-2 box-question'
-          onClick={() => popToggle(randomCock[0].idDrink)}>
+        <div className="filter background-2 box-question" onClick={() => popToggle(randomCock[0].idDrink)}>
           <h2>Zufall</h2>
-          <p className='question'>?</p>
+          <p className="question">?</p>
         </div>
       </section>
 
