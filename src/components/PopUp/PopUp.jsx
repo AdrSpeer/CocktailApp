52b49e599d432b4fcc4b10d5a782253a66e6e68d
icaroboaventura <boaventura@outlook.de>
2024-03-26 11:38:01 -0300

    Add update popUp

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -1,8 +1,8 @@
-import './PopUp.css';
-import { PopContext } from '../../context/Context';
-import { DrinkContext } from '../../context/Context';
-import { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import "./PopUp.css";
+import { PopContext } from "../../context/Context";
+import { DrinkContext } from "../../context/Context";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const PopUp = () => {
   const [drink, setDrink] = useState();
@@ -20,65 +20,51 @@ const PopUp = () => {
 
   useEffect(() => {
     drinkId != null
-      ? fetch(
-          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
-        )
+      ? fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
           .then((res) => res.json())
           .then((data) => {
-            data.drinks != null ? setDrink(data) : console.log('');
+            data.drinks != null ? setDrink(data) : console.log("");
           })
-          .catch((error) => console.log('Fetch Drinks', error))
-      : '';
+          .catch((error) => console.log("Fetch Drinks", error))
+      : "";
   }, [drinkId]);
 
   let ingredients = [];
   const getIngredients = () => {
     for (let item in drink.drinks[0]) {
-      if (item.includes('strIngredient')) {
-        if (
-          typeof drink.drinks[0][item] != 'string' ||
-          drink.drinks[0][item] === ''
-        ) {
+      if (item.includes("strIngredient")) {
+        if (typeof drink.drinks[0][item] != "string" || drink.drinks[0][item] === "") {
         } else {
           ingredients.push(drink.drinks[0][item]);
         }
       }
     }
   };
-  drink ? getIngredients() : '';
+  drink ? getIngredients() : "";
 
   return (
-    <div className='pop-up'>
-      <div className={pop === true ? 'pop pop-show' : 'pop'}>
+    <div className="pop-up">
+      <div className={pop === true ? "pop pop-show" : "pop"}>
+        <img onClick={() => popToggle()} className="back-cross" src="/svg/Cross.svg" alt="" />
         {drink ? (
-          <div className={pop === true ? 'drink-div' : 'drink-div-hide'}>
-            <img
-              onClick={() => popToggle()}
-              className='back-cross'
-              src='/svg/Cross.svg'
-              alt=''
-            />
-            <img
-              className='drink-img'
-              src={drink.drinks[0].strDrinkThumb}
-              alt=''
-            />
-            <div className='content-con'>
-              <h4 className='pop-h4'>{drink.drinks[0].strDrink}</h4>
-              <h5 className='pop-h5'>Ingredients</h5>
+          <div className={pop === true ? "drink-div" : "drink-div-hide"}>
+            <img className="drink-img" src={drink.drinks[0].strDrinkThumb} alt="" />
+            <div className="content-con">
+              <h4 className="pop-h4">{drink.drinks[0].strDrink}</h4>
+              <h5 className="pop-h5">Ingredients</h5>
               {ingredients.length !== 0
                 ? ingredients.map((ingredient, index) => (
-                    <p className='pop-p' key={index}>
+                    <p className="pop-p" key={index}>
                       {ingredient}
                     </p>
                   ))
-                : ''}
-              <h5 className='pop-h5'>Instruction</h5>
-              <p className='pop-p'>{drink.drinks[0].strInstructions}</p>
+                : ""}
+              <h5 className="pop-h5">Instruction</h5>
+              <p className="pop-p">{drink.drinks[0].strInstructions}</p>
             </div>
           </div>
         ) : (
-          ''
+          ""
         )}
       </div>
     </div>
